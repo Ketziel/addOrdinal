b47502a55e439089af9a6259dc385023338a69f5
Richard Wills <richardwills90@gmail.com>
2014-06-17 16:02:53 +0100

    Basic Functionality Completed

---
+++ core/components/hanzel/elements/snippets/snippet.addOrdinal.php
@@ -0,0 +1,12 @@
+<?php
+$lastDigit = $input[strlen($input)-1];
+
+if ($lastDigit == '0' || $lastDigit == '1' || $lastDigit == '4'||$lastDigit == '5'||$lastDigit == '6'||$lastDigit == '7'||$lastDigit == '8'||$lastDigit == '9') {
+	return $input.'<sup>th</sup>';
+} else if ($lastDigit == '2') {
+	return $input.'<sup>nd</sup>';
+} else if ($lastDigit == '3') {
+	return $input.'rd';
+}
+
+return $input;
